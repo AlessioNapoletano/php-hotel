b510a6eb2240f4dbb94aa46a9184985f14734fbb
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-01-25 15:41:34 +0100

    show hotel

--- index.php
+++ index.php
@@ -6,10 +6,87 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Hotel</title>
 
+    <!--CSS-->
+    <link rel="stylesheet" href="./styles/style.css">
+
 </head>
 <body>
+    <?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+    <header>
+        <h1>
+            Hotel
+        </h1>
+
+        <hr>
+    </header>
     <main>
-        
+        <!-- 
+            $hotels = 
+            [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ]
+        -->
+              
+        <!-- Stampare tutti i nostri hotel con tutti i dati disponibili. -->
+
+        <article>
+            <h2>
+                Hotel disponibili: 
+            </h2>
+
+            <?php 
+                foreach($hotels as $hotel) {
+                    echo $hotel;
+                }
+            ?>
+        </article>
     </main>
 </body>
 </html>
